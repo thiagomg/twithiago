4e6196008277667c039537979361ddd32daf7b5b
thiedri <thiedri@8854fccc-d45e-11de-bff8-d51c198f79d0>
2009-11-24 18:39:54 +0000

    added incomplete update and profile picture support

--- twitter.cpp
+++ twitter.cpp
@@ -5,6 +5,7 @@
 
 #include "friendlisthandler.h"
 
+
 void Twitter::setProxy(const QString &user, const QString &pwd)
 {
 	_proxyUser = user;
@@ -53,13 +54,39 @@ bool Twitter::_getTimeline(const QString &url, int count)
 {
 	QString twitterUrl;
 
-        twitterUrl = url + QString("?count=%1").arg( count );
+	twitterUrl = url + QString("?count=%1").arg( count );
 
-	QUrl netUrl( url );
+	QUrl netUrl( twitterUrl );
 
 	_checkProxy(netUrl); //Use proxy ?
 
-	QNetworkRequest request(url);
+	QNetworkRequest request(netUrl);
+
+	QString login( _twitterUser );
+	QString password( _twitterPwd );
+	QByteArray auth = login.toUtf8() + ":" + password.toUtf8();
+	auth = auth.toBase64().prepend( "Basic " );
+
+	request.setRawHeader( "Authorization", auth );
+	request.setAttribute( ATTR_TIPO, _attr_timeline );
+
+	nam->get(request);
+
+	return true;
+}
+
+bool Twitter::postUpdate(const QString &data, quint64 inReplyTo)
+{
+	//TODO: Fazer o máx de requests por minuto
+	QString twitterUrl;
+
+	twitterUrl = _updateUrl;
+
+	QUrl netUrl( twitterUrl );
+
+	_checkProxy(netUrl); //Use proxy ?
+
+	QNetworkRequest request(netUrl);
 
 	QString login( _twitterUser );
 	QString password( _twitterPwd );
@@ -68,17 +95,57 @@ bool Twitter::_getTimeline(const QString &url, int count)
 
 	request.setRawHeader( "Authorization", auth );
 
-	nam->get(request);
+	QByteArray postData( "status=" );
+	postData.append( data.toUtf8().toPercentEncoding() );
+	if ( inReplyTo != 0 ) {
+		postData.append( "&in_reply_to_status_id=" + QByteArray::number( inReplyTo ) );
+	}
+	postData.append( "&source=Twithiago" );
+
+	request.setAttribute( ATTR_TIPO, _attr_update );
+
+	nam->post(request, postData);
 
 	return true;
 }
 
-bool Twitter::_postUpdate()
+
+const QImage *Twitter::getPicture(const QString &userName, const QString &url)
 {
-	return false;
+	QUrl netUrl( url );
+
+	QMap<QString, QImage>::iterator it = _mapPics.find( netUrl.toString(QUrl::None) );
+	if( it != _mapPics.end() ) {
+		QImage &img = *it;
+		return &img;
+	}
+
+	_checkProxy(netUrl); //Use proxy ?
+
+	QNetworkRequest request(netUrl);
+	request.setAttribute( ATTR_TIPO, _attr_picture );
+	request.setAttribute( ATTR_PIC_URL, url );
+	request.setAttribute( ATTR_USER, userName );
+	
+	nam->get(request);
+
+	return NULL;
 }
 
 void Twitter::onNetRecv(QNetworkReply *reply)
+{
+	int tipo = reply->attribute( ATTR_TIPO ).toInt();
+	switch( tipo ) {
+	case _attr_timeline:
+		_processTimeline(reply);
+		break;
+	case _attr_picture:
+		_processPictures(reply);
+		break;
+	}
+}
+
+void Twitter::_processTimeline(QNetworkReply *reply)
 {
 	QVariant statusCodeV = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
 	QVariant redirectionTargetUrl = reply->attribute(QNetworkRequest::RedirectionTargetAttribute);
@@ -107,5 +174,26 @@ void Twitter::onNetRecv(QNetworkReply *reply)
 		emit onFriendsTimelineXML(reply->errorString(), 1);
 		emit onFriendsTimeline(NULL, 1);
 	}
+}
 
+void Twitter::_processPictures(QNetworkReply *reply)
+{
+	QString userName = reply->attribute( ATTR_USER ).toString();
+
+	QString url = reply->url().toString(QUrl::None);
+	qDebug() << "URL: " << url;
+
+	QMap<QString, QImage>::iterator it = _mapPics.find(url);
+	if( it != _mapPics.end() ) {
+		//already there
+		return;
+	}
+
+	QImage img;
+	it = _mapPics.insert( url, img );
+
+	QImage &imgIns = *it;
+	imgIns.loadFromData( reply->readAll() );
+
+	emit onFriendPicture(userName, imgIns);
 }
--- twitter.h
+++ twitter.h
@@ -2,6 +2,7 @@
 
 #include <QtNetwork>
 #include <QObject>
+#include <QImage>
 
 #include "Timeline.h"
 
@@ -17,41 +18,61 @@ public:
 
 		_timelineUrl = "http://twitter.com/statuses/friends_timeline.xml";
 		_mentionsUrl = "http://twitter.com/statuses/mentions.xml";
+		_updateUrl = "http://twitter.com/statuses/update.xml";
 		_directsUrl = "http://twitter.com/direct_messages.xml";
 
 		connect(nam, SIGNAL(finished(QNetworkReply*)), this, SLOT(onNetRecv(QNetworkReply*)));
 	}
 	~Twitter() {}
 
-	bool getFriendsTimeline(int count=20);
-	bool getMentionsTimeline(int count=20);
-	bool getDirectsTimeline(int count=20);
-
 	void setProxy(const QString &user, const QString &pwd);
 	void setTwitterAccount(const QString &user, const QString &pwd);
 
+	bool getFriendsTimeline(int count=20);
+	bool getMentionsTimeline(int count=20);
+	bool getDirectsTimeline(int count=20);
+	const QImage *getPicture(const QString &userName, const QString &url);
+	bool postUpdate(const QString &data, quint64 inReplyTo);
+
 protected:
 	bool _getTimeline(const QString &url, int count);
-	bool _postUpdate();
 	bool _checkProxy(QUrl &netUrl);
+
+	void _processTimeline(QNetworkReply *reply);
+	void _processPictures(QNetworkReply *reply);
 
 signals:
 	void onFriendsTimelineXML(const QString &timeLine, int error);
 	void onFriendsTimeline(Timeline *timeLine, int error);
+	void onFriendPicture(const QString &user, const QImage &img);
 
 private slots:
 	void onNetRecv(QNetworkReply *reply);
+
+private:
+	static const QNetworkRequest::Attribute ATTR_TIPO = (QNetworkRequest::Attribute)(QNetworkRequest::User+1);
+	static const QNetworkRequest::Attribute ATTR_PIC_URL = (QNetworkRequest::Attribute)(QNetworkRequest::User+2);
+	static const QNetworkRequest::Attribute ATTR_USER = (QNetworkRequest::Attribute)(QNetworkRequest::User+3);
+
+	enum {
+		_attr_timeline,
+		_attr_update,
+		_attr_picture
+	};
 
 private:
 	QNetworkAccessManager *nam;
 	QString _timelineUrl;
 	QString _mentionsUrl;
 	QString _directsUrl;
+	QString _updateUrl;
 
 	QString _proxyUser;
 	QString _proxyPwd;
 	QString _twitterUser;
 	QString _twitterPwd;
+
+	QMap<QString, QImage> _mapPics;
 
 };
 
