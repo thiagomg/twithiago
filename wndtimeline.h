8e65cf7fcc2ea13eb74b01f74dd60d20b826cd9d
thiedri <thiedri@8854fccc-d45e-11de-bff8-d51c198f79d0>
2009-12-04 15:08:27 +0000

    links implementado

--- wndtimeline.h
+++ wndtimeline.h
@@ -23,18 +23,24 @@ public:
 protected:
 	QString _changeLinks(const QString &text);
 	int _endTok(const QString &text, int pos);
+	QString _cleanLink(const QString &tok);
+
 	void _setWaiting(bool waiting);
 
 	bool _checkCredentials();
 
 	void _createItem(int pos, const QString &id, const QString &user, const QString &picUrl, const QString &text);
 	void _updateItem(int pos, const QString &id, const QString &user, const QString &picUrl, const QString &text);
+	QString _getItem(int pos);
 
 	const QImage *_getPicture(const QString &user, const QString &picUrl);
 	void _showUpdate();
+	void _checkMsgList();
 
 	//Parsers --------------------------------------------
 	QString _parseReply(const QString &cmd);
+	void _parseTag(const QString &tag);
+	QString _parseRT(const QString &msgNum);
 	//====================================================
 
 private:
@@ -55,6 +61,7 @@ private:
     Ui::WndTimeline *ui;
 	Twitter _twitter;
 	QList< QFrame* > _frameList;
+	QVector< QString > _msgList;
 	TIPO_REQ _tipoReq;
 
 	Credentials _credentials;
@@ -72,6 +79,7 @@ private slots:
 	void onFriendsTimeline(Timeline *timeLine, int error);
 	void onFriendPicture(const QTwitPicture &pic);
 	void onUpdatePressed();
+	void onCancelPressed();
 	void onUpdate(Timeline *timeLine, int error);
 
 	void linkClicked(QString desc);
